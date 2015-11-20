f17b083fbd0ac15d51ec9c3e328a383a43e730eb
Ironn94 <roberto_94_va@hotmail.com>
2015-11-20 12:07:20 +0100

    variados controles del videoPlayer

--- app/src/main/java/es/uva/inf/espectacle/Fragments/VideoPlayerFragment.java
+++ app/src/main/java/es/uva/inf/espectacle/Fragments/VideoPlayerFragment.java
@@ -65,13 +65,14 @@ public class VideoPlayerFragment extends Fragment implements View.OnClickListene
         video.setMinimumWidth(width);
         video.setMinimumHeight(height);
         video.setMediaController(mediaController);
-        bPlay = (ImageButton) view.findViewById(R.id.buttonPlay);
+        /*bPlay = (ImageButton) view.findViewById(R.id.buttonPlay);
         bPlay.setOnClickListener(this);
         bPlay.setImageResource(R.drawable.play_button_selector);
         bNext = (ImageButton) view.findViewById(R.id.buttonNext);
         bNext.setOnClickListener(this);
         bBack = (ImageButton) view.findViewById(R.id.buttonBack);
-        bBack.setOnClickListener(this);
+        bBack.setOnClickListener(this);*/
+        mediaController.show();
         video.setVideoPath(path);
         return view;
     }
@@ -137,13 +138,13 @@ public class VideoPlayerFragment extends Fragment implements View.OnClickListene
 
     @Override
     public void onClick(View v) {
-        if(v.getId()==R.id.buttonPlay){
+        /*if(v.getId()==R.id.buttonPlay){
             onPlayButton();
         }else if(v.getId()==R.id.buttonNext){
             onNextButton();
         }else if(v.getId()==R.id.buttonBack){
             onBackButton();
-        }
+        }*/
     }
 
     private void onBackButton() {
